ec8b257e9348869a0d9b3ed7fad92cd142c2ab5f
3399075402@qq.com <3399075402@qq.com>
2024-10-26 23:12:51 +0800

    day2

--- go_code/main.go
+++ go_code/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	//定义变量的三种方式
+	var i int
+	i = 10
+	fmt.Println("i=", i)
+
+	var num = 10.4
+	fmt.Println("num=", num)
+	//fmt.Printf()可以用于做格式化输出
+	fmt.Printf("num的类型 %T\n", num)
+	fmt.Printf("num占用的字节数是 %d\n", unsafe.Sizeof(num))
+
+	//:=左侧的变量不应该是已经声明过的
+	name := "tom"
+	fmt.Println("name=", name)
 }
