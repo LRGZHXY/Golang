5f1a6e43ddcbe07a9fc71de0bd02ed8197dad250
3399075402@qq.com <3399075402@qq.com>
2024-10-30 21:13:19 +0800

    day5

---
+++ struct/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type Teacher struct {
+	//变量名字大写外界可以访问这个属性
+	Name   string
+	Age    int
+	School string
+}
+
+func main() {
+	//创建结构体实例的几种方式
+	var t1 Teacher
+
+	var t2 Teacher = Teacher{"小明", 19, "西安邮电大学"}
+
+	var t3 *Teacher = new(Teacher)
+
+	var t4 *Teacher = &Teacher{"小青", 13, "西安财经大学"}
+
+	fmt.Println(t1)
+	fmt.Println(t2)
+	fmt.Println(t3)
+	fmt.Println(t4)
+
+}
