911d87b2cf8bff867f5101d07629c56b9c0aac6a
LRGZHXY <3399075402@qq.com>
2025-03-05 23:06:23 +0800

    6.failed

--- bluebell/controllers/user.go
+++ bluebell/controllers/user.go
@@ -11,18 +11,28 @@ import (
 
 func SignUpHandler(c *gin.Context) {
 	//1.获取参数和参数校验
-	var p models.ParamSignUp
+	p := new(models.ParamSignUp)
 	if err := c.ShouldBindJSON(&p); err != nil {
 		//请求参数有误,直接返回响应
 		zap.L().Error("SignUp with invalid param", zap.Error(err))
 		c.JSON(http.StatusOK, gin.H{
-			"msg": "请求参数有误",
+			"msg": err.Error(),
 		})
 		return
 	}
+
+	//手动对请求参数进行详细的业务规则校验
+	/*if len(p.Username) == 0 || len(p.Password) == 0 || len(p.RePassWord) == 0 || p.Password != p.Password {
+		zap.L().Error("SignUp with invalid param")
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "请求参数有误",
+		})
+		return
+	}*/
+
 	fmt.Println(p)
 	//2.业务处理
-	logic.SignUp()
+	logic.SignUp(p)
 	//3.返回响应
 	c.JSON(http.StatusOK, gin.H{
 		"msg": "success",
--- bluebell/logic/user.go
+++ bluebell/logic/user.go
@@ -2,12 +2,13 @@ package logic
 
 import (
 	"bluebell/dao/mysql"
+	"bluebell/models"
 	"bluebell/pkg/snowflake"
 )
 
 //存放业务逻辑的代码
 
-func SignUp() {
+func SignUp(p *models.ParamSignUp) {
 	//1.判断用户存不存在
 	mysql.QueryUserByUsername()
 
