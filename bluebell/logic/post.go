adcd51124897872e029bf42f2d4b45a6adc86a67
LRGZHXY <3399075402@qq.com>
2025-03-11 21:00:33 +0800

    获取帖子详情2

--- bluebell/logic/post.go
+++ bluebell/logic/post.go
@@ -4,6 +4,8 @@ import (
 	"bluebell/dao/mysql"
 	"bluebell/models"
 	"bluebell/pkg/snowflake"
+
+	"go.uber.org/zap"
 )
 
 func CreatePost(p *models.Post) (err error) {
@@ -15,6 +17,36 @@ func CreatePost(p *models.Post) (err error) {
 }
 
 // 根据帖子id查询帖子详情数据
-func GetPostById(pid int64) (data *models.Post, err error) {
-	return mysql.GetPostById(pid)
+func GetPostById(pid int64) (data *models.ApiPostDetail, err error) {
+	//查询并组合接口想用的数据
+	post, err := mysql.GetPostById(pid)
+	if err != nil {
+		zap.L().Error("mysql.GetPostById(pid) failed",
+			zap.Int64("pid", pid),
+			zap.Error(err))
+	}
+
+	//根据作者id查作者信息
+	user, err := mysql.GetUserById(post.AuthorID)
+	if err != nil {
+		zap.L().Error("mysql.GetUserById(post.AuthorID) failed",
+			zap.Int64("post.AuthorID", post.AuthorID),
+			zap.Error(err))
+		return
+	}
+	//根据社区id查询社区详细信息
+	community, err := mysql.GetCommunityDetailByID(post.CommunityID)
+	if err != nil {
+		zap.L().Error("mysql.GetCommunityDetailByID(post.CommunityID) failed",
+			zap.Int64("post.CommunityID", post.CommunityID),
+			zap.Error(err))
+		return
+	}
+	//接口数据拼接
+	data = &models.ApiPostDetail{
+		AuthorName:      user.Username,
+		Post:            post,
+		CommunityDetail: community,
+	}
+	return
 }
--- bluebell/models/post.go
+++ bluebell/models/post.go
@@ -12,3 +12,10 @@ type Post struct {
 	Status      int32     `json:"status" db:"status"`
 	CreateTime  time.Time `json:"create_time" db:"create_time"`
 }
+
+// 帖子详情接口的结构体
+type ApiPostDetail struct {
+	AuthorName       string             `json:"author_name"`
+	*Post                               //嵌入帖子结构体
+	*CommunityDetail `json:"community"` //嵌入社区信息
+}
