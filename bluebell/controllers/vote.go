b6f7bab8e33b664eb1faf1c6e95a35448edcacc7
LRGZHXY <3399075402@qq.com>
2025-03-12 09:59:19 +0800

    分析投票逻辑

---
+++ bluebell/controllers/vote.go
@@ -0,0 +1,40 @@
+package controllers
+
+import (
+	"bluebell/logic"
+	"bluebell/models"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+// 投票
+/*type VoteData struct {
+	//UserID 从请求中获取当前的用户
+	PostID    int64 `json:"post_id,string"`   //帖子id
+	Direction int   `json:"direction,string"` //赞成票（1）还是反对票（-1）
+}*/
+
+func PostVoteController(c *gin.Context) {
+	//参数校验
+	p := new(models.ParamVoteData)
+	if err := c.ShouldBind(&p); err != nil {
+		errs, ok := err.(validator.ValidationErrors) //类型断言
+		if !ok {
+			ResponseError(c, CodeInvalidParam)
+			return
+		}
+		errData := removeTopStruct(errs.Translate(trans)) //翻译并去除错误提示中的结构体标识
+		ResponseErrorWithMsg(c, CodeInvalidParam, errData)
+		return
+	}
+	//获取当前请求用户的id
+	userID, err := getCurrentUserID(c)
+	if err != nil {
+		ResponseError(c, CodeNeedLogin)
+		return
+	}
+	//具体投票的业务逻辑
+	logic.VoteForPost(userID, p)
+	ResponseSuccess(c, nil)
+}
--- bluebell/logic/vote.go
+++ bluebell/logic/vote.go
@@ -1,1 +1,27 @@
 package logic
+
+import "bluebell/models"
+
+//投票功能
+
+/*投票的几种情况：
+direction=1：
+	1.之前没有投过票，现在投赞成票
+	2.之前投反对票，现在改为赞成票
+direction=0：
+	1.之前投赞成票，现在取消投票
+	2.之前投反对票，现在取消投票
+direction=-1：
+	1.之前没有投过票，现在投反对票
+	2.之前投赞成票，现在改为反对票
+
+投票的限制：
+每个帖子自发表之日起一个星期之内允许用户投票，超过一个星期不允许投票
+	1.到期之后将redis中保存的赞成票及反对票存储到mysql表中
+	2.到期之后删除 KeyPostVotedZSetPF
+*/
+
+// VoteForPost 为帖子投票的函数
+func VoteForPost(userID int64, p *models.ParamVoteData) {
+
+}
