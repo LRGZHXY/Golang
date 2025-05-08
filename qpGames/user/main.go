9503cbb54f3f84f487e23dcaf883ac4ba00824bd
LRGZHXY <3399075402@qq.com>
2025-05-08 22:02:04 +0800

    加载配置&启动监控

---
+++ qpGames/user/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"common/config"
+	"common/metrics"
+	"flag"
+	"fmt"
+)
+
+var configFile = flag.String("config", "application.yaml", "config file")
+
+func main() {
+	//1.加载配置
+	flag.Parse()
+	config.InitConfig(*configFile)
+
+	//2.启动监控
+	go func() {
+		err := metrics.Server(fmt.Sprintf("0.0.0.0:%d", config.Conf.MetricPort))
+		if err != nil {
+			panic(err)
+		}
+	}()
+	//3.启动grpc服务端
+	select {}
+}
