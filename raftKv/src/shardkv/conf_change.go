e571761ba6ea3edb42e2f037971c301d4a0077b0
LRGZHXY <3399075402@qq.com>
2025-06-18 22:01:20 +0800

    配置变更

--- raftKv/src/shardkv/conf_change.go
+++ raftKv/src/shardkv/conf_change.go
@@ -6,10 +6,10 @@ import (
 )
 
 func (kv *ShardKV) ConfigCommand(commnd RaftCommand, reply *OpReply) {
-	// 调用 raft，将请求存储到 raft 日志中并进行同步
+	// 调用raft，将请求存储到raft日志中并进行同步
 	index, _, isLeader := kv.rf.Start(commnd)
 
-	// 如果不是 Leader 的话，直接返回错误
+	// 如果不是Leader的话，直接返回错误
 	if !isLeader {
 		reply.Err = ErrWrongLeader
 		return
@@ -35,6 +35,7 @@ func (kv *ShardKV) ConfigCommand(commnd RaftCommand, reply *OpReply) {
 	}()
 }
 
+// handleConfigChangeMessage 接收配置变更命令
 func (kv *ShardKV) handleConfigChangeMessage(command RaftCommand) *OpReply {
 	switch command.CmdType {
 	case ConfigChange:
@@ -51,18 +52,19 @@ func (kv *ShardKV) handleConfigChangeMessage(command RaftCommand) *OpReply {
 	}
 }
 
+// applyConfigChange 处理配置变更
 func (kv *ShardKV) applyNewConfig(newConfig shardctrler.Config) *OpReply {
 	if kv.currentConfig.Num+1 == newConfig.Num {
 		for i := 0; i < shardctrler.NShards; i++ {
 			if kv.currentConfig.Shards[i] != kv.gid && newConfig.Shards[i] == kv.gid {
-				//	shard 需要迁移进来
+				//当前配置中分片不属于本组，新配置中分片属于本组 -> shard需要迁移进来
 				gid := kv.currentConfig.Shards[i]
 				if gid != 0 {
 					kv.shards[i].Status = MoveIn
 				}
 			}
 			if kv.currentConfig.Shards[i] == kv.gid && newConfig.Shards[i] != kv.gid {
-				// shard 需要迁移出去
+				//当前配置中分片属于本组，新的配置中属于其他组 -> shard需要迁移出去
 				gid := newConfig.Shards[i]
 				if gid != 0 {
 					kv.shards[i].Status = MoveOut
@@ -76,16 +78,17 @@ func (kv *ShardKV) applyNewConfig(newConfig shardctrler.Config) *OpReply {
 	return &OpReply{Err: ErrWrongConfig}
 }
 
+// applyShardMigration 处理分片迁移
 func (kv *ShardKV) applyShardMigration(shardDataReply *ShardOperationReply) *OpReply {
 	if shardDataReply.ConfigNum == kv.currentConfig.Num {
 		for shardId, shardData := range shardDataReply.ShardData {
 			shard := kv.shards[shardId]
-			// 将数据存储到当前 Group 对应的 shard 中
+			// 将数据存储到当前Group对应的shard中
 			if shard.Status == MoveIn {
 				for k, v := range shardData {
 					shard.KV[k] = v
 				}
-				// 状态置为 GC，等待清理
+				// 状态置为GC，等待清理
 				shard.Status = GC
 			} else {
 				break
@@ -95,7 +98,7 @@ func (kv *ShardKV) applyShardMigration(shardDataReply *ShardOperationReply) *OpR
 		// 拷贝去重表数据
 		for clientId, dupTable := range shardDataReply.DuplicateTable {
 			table, ok := kv.duplicateTable[clientId]
-			if !ok || table.SeqId < dupTable.SeqId {
+			if !ok || table.SeqId < dupTable.SeqId { //迁移过来的信息更"新"
 				kv.duplicateTable[clientId] = dupTable
 			}
 		}
@@ -103,6 +106,7 @@ func (kv *ShardKV) applyShardMigration(shardDataReply *ShardOperationReply) *OpR
 	return &OpReply{Err: ErrWrongConfig}
 }
 
+// applyShardGC 处理分片GC
 func (kv *ShardKV) applyShardGC(shardsInfo *ShardOperationArgs) *OpReply {
 	if shardsInfo.ConfigNum == kv.currentConfig.Num {
 		for _, shardId := range shardsInfo.ShardIds {
@@ -110,7 +114,7 @@ func (kv *ShardKV) applyShardGC(shardsInfo *ShardOperationArgs) *OpReply {
 			if shard.Status == GC {
 				shard.Status = Normal
 			} else if shard.Status == MoveOut {
-				kv.shards[shardId] = NewMemoryKVStateMachine()
+				kv.shards[shardId] = NewMemoryKVStateMachine() //创建一个新的、空的状态机对象 = 把分片数据清空
 			} else {
 				break
 			}
