a53e16c6e8c4d0d91988b0a46939ec08a3da6933
LRGZHXY <3399075402@qq.com>
2025-07-15 10:10:46 +0800

    arr1

--- LeetCode/practice/test2.go
+++ LeetCode/practice/test2.go
@@ -1,1 +1,22 @@
 package practice
+
+func sortedSquares(nums []int) []int {
+	n := len(nums)
+	i, j, k := 0, n-1, n-1
+	result := make([]int, n)
+	for i <= j {
+		left := nums[i] * nums[i]
+		right := nums[j] * nums[j]
+		if left < right {
+			result[k] = right
+			k--
+			j--
+		}
+		if left >= right {
+			result[k] = left
+			k--
+			i++
+		}
+	}
+	return result
+}
