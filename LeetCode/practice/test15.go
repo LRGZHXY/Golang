ae319603678af12903084b4d89c9cec99ee7234e
LRGZHXY <3399075402@qq.com>
2025-07-23 17:30:50 +0800

    str2

---
+++ LeetCode/practice/test15.go
@@ -0,0 +1,33 @@
+package practice
+
+func strStr(haystack string, needle string) int {
+	next := make([]int, len(needle))
+	getNext(next, needle)
+	j := 0
+	for i := 0; i < len(haystack); i++ {
+		for j > 0 && haystack[i] != needle[j] {
+			j = next[j-1]
+		}
+		if haystack[i] == needle[j] {
+			j++
+		}
+		if j == len(needle) {
+			return i - len(needle) + 1
+		}
+	}
+	return -1
+}
+
+func getNext(next []int, s string) {
+	j := 0
+	next[0] = 0
+	for i := 1; i < len(s); i++ {
+		for j > 0 && s[i] != s[j] {
+			j = next[j-1]
+		}
+		if s[i] == s[j] {
+			j++
+		}
+		next[i] = j
+	}
+}
